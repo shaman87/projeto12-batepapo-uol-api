8f69f3d2011db35c61b6c44fd4b1341e131e32b2
Felipe <rockalbuquerque@gmail.com>
2022-09-05 03:13:44 -0300

    Cadastrar participante

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import express from "express";
 import cors from "cors";
 import { MongoClient, ObjectId } from "mongodb";
+import joi from "joi";
+import dayjs from "dayjs";
 import dotenv from "dotenv";
+
 dotenv.config();
 
 const app = express();
@@ -11,12 +14,45 @@ app.use(express.json());
 const mongoClient = new MongoClient(process.env.MONGO_URI);
 let db;
 
-mongoClient.connect()
-    .then(() => {
-        db = mongoClient.db("chat-uol");
-    })
-    .catch((error) => {
-        console.error(error);
-    });
+mongoClient.connect().then(() => {
+    db = mongoClient.db("chat-uol");
+});
+
+const participantsSchema = joi.object({
+    name: joi.string().empty(" ").required()
+});
+
+app.post("/participants", async (req, res) => {
+    const { name } = req.body;
+
+    const validation = participantsSchema.validate({ name }, { abortEarly: false });
+
+    if (validation.error) {
+        const errorList = validation.error.details.map(error => error.message);
+        return res.status(422).send(errorList);
+    }
+
+    try {
+        const participantsList = await db.collection("participants").findOne({ name });
+
+        if (participantsList) {
+            return res.status(409).send("Participante já cadastrado!");
+        } else {
+            await db.collection("participants").insertOne({ name, lastStatus: Date.now() });
+
+            await db.collection("messages").insertOne({
+                from: name, 
+                to: "Todos", 
+                text: "entra na sala", 
+                type: "status", 
+                time: dayjs().format("HH:mm:ss")
+            });
+
+            res.sendStatus(201);
+        }
+    } catch(error) {
+        res.status(500).send(error.message);
+    }
+});
 
 app.listen(5000, () => console.log("Listening on port 5000"));
