fb26d1a9c02cb0387c345562bebefbdfbb8c1f77
Felipe <rockalbuquerque@gmail.com>
2022-09-05 04:59:24 -0300

    Enviar mensagens

--- index.js
+++ index.js
@@ -18,25 +18,31 @@ mongoClient.connect().then(() => {
     db = mongoClient.db("chat-uol");
 });
 
-const participantsSchema = joi.object({
+const participantSchema = joi.object({
     name: joi.string().empty(" ").required()
+});
+
+const messageSchema = joi.object({
+    to: joi.string().empty(" ").required(), 
+    text: joi.string().empty(" ").required(), 
+    type: joi.string().valid("message", "private_message").required()
 });
 
 app.post("/participants", async (req, res) => {
     const { name } = req.body;
 
-    const validation = participantsSchema.validate({ name }, { abortEarly: false });
+    const validation = participantSchema.validate({ name }, { abortEarly: false });
 
     if (validation.error) {
         const errorList = validation.error.details.map(error => error.message);
-        return res.status(422).send(errorList);
-    }
+        res.status(422).send(errorList);
+    };
 
     try {
         const participantsList = await db.collection("participants").findOne({ name });
 
         if (participantsList) {
-            return res.status(409).send("Participante já cadastrado!");
+            res.status(409).send("Participante já cadastrado!");
         } else {
             await db.collection("participants").insertOne({ name, lastStatus: Date.now() });
 
@@ -52,7 +58,7 @@ app.post("/participants", async (req, res) => {
         }
     } catch(error) {
         res.status(500).send(error.message);
-    }
+    };
 });
 
 app.get("/participants", async (req, res) => {
@@ -61,7 +67,39 @@ app.get("/participants", async (req, res) => {
         res.send(participantsList);
     } catch(error) {
         res.sendStatus(500);
-    }
+    };
+});
+
+app.post("/messages", async (req, res) => {
+    const { to, text, type } = req.body;
+    const { user } = req.headers;
+
+    const validation = messageSchema.validate({ to, text, type }, { abortEarly: false });
+
+    if (validation.error) {
+        const errorList = validation.error.details.map(error => error.message);
+        res.status(422).send(errorList);
+    };
+
+    try {
+        const participantExist = await db.collection("participants").findOne({ name: user });
+
+        if (!participantExist) {
+            res.status(422).send("Participante não não encontrado!");
+        } else {
+            await db.collection("messages").insertOne({
+                from: user, 
+                to, 
+                text, 
+                type, 
+                time: dayjs().format("HH:mm:ss")
+            });
+
+            res.sendStatus(201);
+        }
+    } catch(error) {
+        res.status(500).send(error.message);
+    };
 });
 
 app.listen(5000, () => console.log("Listening on port 5000"));
